da5ee73e8f529170a3d6436dc2d0caa5f4f64d9c
Robert Sandh <sarobban@gmail.com>
2020-11-18 12:37:03 +0100

    Old prototype and new InputManager

---
+++ CambleFallTesting/Assets/Scripts/RobbansSkräp/test.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class test : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 control = Vector3.right * Input.GetAxis("HorizontalPl1");
+        transform.position += control * Time.deltaTime;
+    }
+}
