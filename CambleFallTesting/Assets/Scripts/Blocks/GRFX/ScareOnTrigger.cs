16a102734c26c807585e0663a4551c46da851156
Robert Sandh <sarobban@gmail.com>
2020-11-26 11:07:23 +0100

    Faceial expression

---
+++ CambleFallTesting/Assets/Scripts/Blocks/GRFX/ScareOnTrigger.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class ScareOnTrigger : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.GetComponent<BlockExpression>())
+        {
+            collision.GetComponent<BlockExpression>().SetMoodScared();
+        }
+    }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.GetComponent<BlockExpression>())
+        {
+            collision.GetComponent<BlockExpression>().SetMoodIdle();
+        }
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
