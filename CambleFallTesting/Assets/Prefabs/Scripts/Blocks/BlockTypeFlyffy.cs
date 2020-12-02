0ccbc33fe20dc26a6c85bd35af9f216710c2211d
Robert Sandh <sarobban@gmail.com>
2020-12-02 21:30:38 +0100

    speedy

--- CambleFallTesting/Assets/Prefabs/Scripts/Blocks/BlockTypeFlyffy.cs
+++ CambleFallTesting/Assets/Prefabs/Scripts/Blocks/BlockTypeFlyffy.cs
@@ -4,6 +4,16 @@ using UnityEngine;
 
 public class BlockTypeFlyffy : BlockType
 {
+    /*
+    protected override void OnInsideTrigger(Collider2D collider)
+    {
+        base.OnInsideTrigger(collider);
+        Vector2 dir = collider.transform.position - transform.position;
+        float dist = dir.sqrMagnitude -1;
+
+        GetComponent<Rigidbody2D>().AddForce(dir.normalized * 5 * dist);
+    }
+    */
     protected override void OnHitEnter(Collision2D collision)
     {
         base.OnHitEnter(collision);
--- CambleFallTesting/Assets/Prefabs/Scripts/Canon/Projectile/SpeedyCube.cs
+++ CambleFallTesting/Assets/Prefabs/Scripts/Canon/Projectile/SpeedyCube.cs
@@ -4,12 +4,34 @@ using UnityEngine;
 
 public class SpeedyCube : Projectile
 {
+    public GameObject fragment;
+    public float exForce = 5;
+
     public float velMulti = 1.4f;
     protected override void Start()
     {
         base.Start();
         rb.velocity *= velMulti;
     }
+
+    protected override void HitEffekt(Collision2D collision)
+    {
+        Vector2 dir = GetComponent<Rigidbody2D>().velocity;
+
+        for (int i = 0; i < 4; i++)
+        {
+            Vector3 plusPos = Quaternion.Euler(0, 0, 90 * i) * Vector3.one;
+            plusPos.z = 0;
+            plusPos *= 0.25f;
+
+            Vector2 scatter = Random.insideUnitSphere;
+            scatter += dir;
+            GameObject frag = Instantiate(fragment, transform.position + plusPos, transform.rotation);
+            frag.GetComponent<Rigidbody2D>().velocity = scatter * exForce;
+        }
+        Destroy(gameObject);
+    }
+
     public override void PlayLaunchSound()
     {
         SoundManager.PlaySound(SoundManager.Sound.FastBlockShoot, transform.position);
