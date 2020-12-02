cfa6e07c82dbba870703069161e6bb9befb0af2e
Robert Sandh <sarobban@gmail.com>
2020-12-02 11:29:43 +0100

    speed script

---
+++ CambleFallTesting/Assets/Prefabs/Art/ChangeLayer.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeLayer : MonoBehaviour
+{
+    public int moveLayers = -10;
+    public AnimationCurve ani;
+    // Start is called before the first frame update
+    void Start()
+    {
+        SpriteRenderer[] changeThis = gameObject.GetComponentsInChildren<SpriteRenderer>();
+        foreach(SpriteRenderer sr in changeThis)
+        {
+            sr.sortingOrder += moveLayers;
+        }
+    }
+}
