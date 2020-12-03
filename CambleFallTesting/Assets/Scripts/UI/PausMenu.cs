61d63cc161a94235b7a0cd23e6a4a1922009ee1d
Lars Olsson <70263953+Gejmerbynight@users.noreply.github.com>
2020-12-03 09:54:28 +0100

    Pausemenu tillagd i prefabs
    Nu finns Pausemenu i prefabs/UI.
    Glöm inte att dra in den i game scene Jacob!

---
+++ CambleFallTesting/Assets/Scripts/UI/PausMenu.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using UnityEditor.UI;
+
+public class PausMenu : MonoBehaviour
+{
+    public static bool GameIsPaused = false;
+    public GameObject PauseMenu; 
+    
+
+    void Update()
+    {
+        if (Input.GetKeyDown("p"))
+        {
+            if (GameIsPaused)
+            {
+                Resume();
+            }
+            
+            else
+            {
+                Pause();
+            }
+        }
+    }
+    public void Resume ()
+    {
+        Time.timeScale = 1f; 
+        Debug.Log ("Resume game");
+        PauseMenu.SetActive(false);
+        GameIsPaused = false;
+    }
+
+    public void Quit()
+    {   
+        Debug.Log ("quit!");
+        Application.Quit();
+    }
+
+    public void Options()
+    {   
+        Debug.Log ("Options");
+        
+    }
+
+    public void ResetScene()
+    {
+        SceneManager.LoadSceneAsync(SceneManager.GetActiveScene().buildIndex);        
+        //PauseMenu.SetActive(false);
+        Time.timeScale = 1f;
+        GameIsPaused = false;
+    }
+
+    void Pause ()
+    {
+        Debug.Log ("Pause");
+        PauseMenu.SetActive(true);
+        Time.timeScale = 0f;
+        GameIsPaused = true;
+    }
+}
