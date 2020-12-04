7e2c1a401f568b767556f56e41601badf7f49066
cseehuusen <cseehuusen@gmail.com>
2020-12-04 11:31:08 +0100

    Menu_Fixed_Placements_Menu_Click_sounds_all_Work_Slight_Delay

---
+++ CambleFallTesting/Assets/ClickSound.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Button))]
+
+public class ClickSound : MonoBehaviour
+{
+
+    public AudioClip sound;
+    private Button button { get { return GetComponent<Button>(); } }
+    private static AudioSource soundPlay;
+
+    void Start()
+    {
+        button.onClick.AddListener(() => PlaySound());
+    }
+
+    void PlaySound()
+    {
+        soundPlay = Instantiate(gameObject.AddComponent<AudioSource>());
+        soundPlay.clip = sound;
+        soundPlay.playOnAwake = false;
+        soundPlay.PlayOneShot(sound);
+        DestroyObject(soundPlay, 0.5f);
+    }
+}
