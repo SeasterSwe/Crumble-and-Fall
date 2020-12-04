1917fcdeabeb116fca2fe803147ceb8a7168fcf9
Robert Sandh <sarobban@gmail.com>
2020-12-04 09:55:39 +0100

    updadate BlockType

--- CambleFallTesting/Assets/Scripts/Blocks/BlockType.cs
+++ CambleFallTesting/Assets/Scripts/Blocks/BlockType.cs
@@ -60,13 +60,6 @@ public class BlockType : MonoBehaviour
 
     }
 
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        //BlockManager.AddBlockToList(gameObject);
-    }
-
     public static bool IsFluffy(GameObject checkObject)
     {
         if (checkObject.GetComponent<BlockType>())
