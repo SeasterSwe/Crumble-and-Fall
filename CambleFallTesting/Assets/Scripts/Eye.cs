390d465742057d4a1b2843586957768fc06a116e
cseehuusen <cseehuusen@gmail.com>
2020-12-07 14:07:09 +0100

    Eye_Fix_FixedMeny

---
+++ CambleFallTesting/Assets/Scripts/Eye.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Eye : MonoBehaviour
+{
+    public Transform eyeWhite;
+    public Transform pupil;
+    public float pupilDistance = 1;
+
+    void Update()
+    {
+        //Vector3 targetDir = Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position;
+        //float angle = (Mathf.Atan2(targetDir.y, targetDir.x) * Mathf.Rad2Deg) - 90f;
+        //transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+
+        RobbansLösning();
+    }
+
+    void RobbansLösning()
+    {
+        Vector3 targetDir = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        
+
+        pupil.position = (targetDir - eyeWhite.position).normalized * pupilDistance;
+        pupil.position = Vector3.Scale(pupil.position, eyeWhite.localScale);
+        pupil.position += eyeWhite.position;
+    }
+
+
+}
+
