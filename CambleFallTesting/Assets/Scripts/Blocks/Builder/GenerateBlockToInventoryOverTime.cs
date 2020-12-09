6d7c62771dc7bc0e1c308f0e0eae5075a8d9d67a
Robert Sandh <sarobban@gmail.com>
2020-12-09 14:51:47 +0100

    Inventory get icon

--- CambleFallTesting/Assets/Scripts/Blocks/Builder/GenerateBlockToInventoryOverTime.cs
+++ CambleFallTesting/Assets/Scripts/Blocks/Builder/GenerateBlockToInventoryOverTime.cs
@@ -7,12 +7,21 @@ public class GenerateBlockToInventoryOverTime : MonoBehaviour
     [Header("Generate")]
     public bool generatorActive = true;
     public float timeBetweenGenerations = 10;
+
+    public float minBonus = 1;
+    public float maxBonus = 10;
+    public float bonus;
+
     private float timeLeftToNextGen;
 
     public Inventory inventory;
 
     // public enum blockType {red, green, blue};
     public BlockType.types currentBlock = BlockType.types.Fluffy;
+
+    public ElevationCheck evCheck;
+    public Blockbuilder blockbuilder;
+    
 
     // Start is called before the first frame update
     void Start()
@@ -21,15 +30,46 @@ public class GenerateBlockToInventoryOverTime : MonoBehaviour
         {
             inventory = GetComponent<Inventory>();
         }
+
+        ElevationCheck[] evCheckers = FindObjectsOfType<ElevationCheck>();
+
+        foreach(ElevationCheck evc in evCheckers)
+        {
+            if (GetComponent<RectTransform>().localPosition.x < 0 && evc.transform.position.x < 0)
+                evCheck = evc;
+
+            if (GetComponent<RectTransform>().localPosition.x > 0 && evc.transform.position.x > 0)
+                evCheck = evc;
+        }
+
+
+        Blockbuilder[] bbs = FindObjectsOfType<Blockbuilder>();
+
+        foreach(Blockbuilder bs in bbs)
+        {
+            if (GetComponent<RectTransform>().localPosition.x < 0 && bs.transform.position.x < 0)
+                blockbuilder = bs;
+
+            if (GetComponent<RectTransform>().localPosition.x > 0 && bs.transform.position.x > 0)
+                blockbuilder = bs;
+        }
+
         timeLeftToNextGen = timeBetweenGenerations;
     }
 
+    void GetBonus()
+    {
+        bonus = evCheck.towerHight / blockbuilder.maxHeight;
+    }
     // Update is called once per frame
     void Update()
     {
+        GetBonus();
+
+
         if (GameState.currentState == GameState.gameStates.Fight)
         {
-            timeLeftToNextGen -= Time.deltaTime;
+            timeLeftToNextGen -= Mathf.Lerp(minBonus, maxBonus, bonus) * Time.deltaTime ;
             if (timeLeftToNextGen < 0)
             {
                 GenerateBlockToInventory();
