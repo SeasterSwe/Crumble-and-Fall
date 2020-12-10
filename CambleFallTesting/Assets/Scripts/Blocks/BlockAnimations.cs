7b06dbbe4949504039c4200a0877ab6eaf429d48
Robert Sandh <sarobban@gmail.com>
2020-12-10 13:14:04 +0100

    upt.

---
+++ CambleFallTesting/Assets/Scripts/Blocks/BlockAnimations.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(BlockType))]
+
+public class BlockAnimations : MonoBehaviour
+{
+    public Animator anim;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+        //bt = GetComponent<BlockType>();
+        anim.SetInteger("State", 0);
+    }
+
+    // Update is called once per frame
+    public void SetAnimation(int i)
+    {
+        print("Called");
+        anim.SetInteger("State", i);
+    }
+}
