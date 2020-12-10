1729d26f6aaa2cdaf0ac7677014b6708316a0977
Robert Sandh <sarobban@gmail.com>
2020-12-10 14:16:25 +0100

    Update BlockType.cs

--- CambleFallTesting/Assets/Scripts/Blocks/BlockType.cs
+++ CambleFallTesting/Assets/Scripts/Blocks/BlockType.cs
@@ -3,7 +3,6 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-[RequireComponent(typeof (BlockAnimations))]
 //using UnityEngine.UI;
 public class BlockType : MonoBehaviour
 {
