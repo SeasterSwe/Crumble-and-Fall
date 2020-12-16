5c777b39c03f4e6a10c917adcb7ce7bf24086f51
cseehuusen <cseehuusen@gmail.com>
2020-12-16 15:22:14 +0100

    Fixed_Volume_Fixed_Menu_Fixed_Paus

---
+++ CambleFallTesting/Assets/SetVolume.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.UI;
+
+
+// Gör så att slider funkar i alla scens och inte bara menu. 
+public class SetVolume : MonoBehaviour
+{
+    public AudioMixer musicMixer;
+    public Slider slider;
+
+    void Start()
+    {
+        slider.value = PlayerPrefs.GetFloat("MusicVolume", 0.75f);
+    }
+    public void SetLevel(float sliderValue)
+    {
+        musicMixer.SetFloat("MusicVolume", Mathf.Log10(sliderValue) * 25);
+        PlayerPrefs.SetFloat("MusicVolume", sliderValue);
+    }
+}
