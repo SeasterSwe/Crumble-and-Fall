2e1d50c90cb951a5faad3ab7c8083f116dc97a4f
cseehuusen <cseehuusen@gmail.com>
2020-12-21 11:43:05 +0100

    Fixed_Back_Esc_Fixed_Movement

---
+++ CambleFallTesting/Assets/BackButton.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class BackButton : MonoBehaviour
+{
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            EventSystem eventSystem = GameObject.Find("EventSystem").GetComponent<EventSystem>();
+            ExecuteEvents.Execute(GetComponent<Button>().gameObject, new BaseEventData(eventSystem), ExecuteEvents.submitHandler);
+        }
+    }
+}
