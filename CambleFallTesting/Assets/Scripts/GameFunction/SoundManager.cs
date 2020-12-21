6d8b94cd864948d916cf596488fe05f16707fe02
Robert Sandh <sarobban@gmail.com>
2020-12-21 11:51:33 +0100

    Update SoundManager.cs

--- CambleFallTesting/Assets/Scripts/GameFunction/SoundManager.cs
+++ CambleFallTesting/Assets/Scripts/GameFunction/SoundManager.cs
@@ -36,8 +36,33 @@ public static class SoundManager
 
         AudioMixer audioMixer = Resources.Load<AudioMixer>("SoundEffectMixer");
         AudioMixerGroup[] audioMixGroup = audioMixer.FindMatchingGroups("Master");
+        audioSource.pitch = 1;
         audioSource.outputAudioMixerGroup = audioMixGroup[0];
     }
+
+    public static void PlaySound(Sound sound, Vector3 position, float pitch)
+    {
+        GameObject soundObj = new GameObject("Sound");
+        soundObj.transform.position = position;
+        AudioSource audioSource = soundObj.AddComponent<AudioSource>();
+
+        //stats från YT blev ganska nice
+        audioSource.clip = GetAudioClip(sound);
+        audioSource.maxDistance = 100f;
+        audioSource.spatialBlend = 1;
+        audioSource.rolloffMode = AudioRolloffMode.Linear;
+
+        audioSource.dopplerLevel = 0f;
+        audioSource.Play();
+        Object.Destroy(soundObj, audioSource.clip.length);
+
+        AudioMixer audioMixer = Resources.Load<AudioMixer>("SoundEffectMixer");
+        AudioMixerGroup[] audioMixGroup = audioMixer.FindMatchingGroups("Master");
+        audioSource.pitch = pitch;
+        audioSource.outputAudioMixerGroup = audioMixGroup[0];
+    }
+
+
     public static void PlaySound(Sound sound)
     {
         if(oneShotGameObject == null)
@@ -49,9 +74,26 @@ public static class SoundManager
             oneShotAudioSource.outputAudioMixerGroup = audioMixGroup[0];
 
         }
-
+        oneShotAudioSource.pitch = 1;
         oneShotAudioSource.PlayOneShot(GetAudioClip(sound));
     }
+
+    public static void PlaySound(Sound sound, float pitch)
+    {
+        if (oneShotGameObject == null)
+        {
+            oneShotGameObject = new GameObject("Sound");
+            oneShotAudioSource = oneShotGameObject.AddComponent<AudioSource>();
+            AudioMixer audioMixer = Resources.Load<AudioMixer>("SoundEffectMixer");
+            AudioMixerGroup[] audioMixGroup = audioMixer.FindMatchingGroups("Master");
+            oneShotAudioSource.outputAudioMixerGroup = audioMixGroup[0];
+
+        }
+
+        oneShotAudioSource.pitch = pitch;
+        oneShotAudioSource.PlayOneShot(GetAudioClip(sound));
+    }
+
     private static AudioClip GetAudioClip(Sound sound)
     {
         foreach (GameAssets.SoundAudioClip audioClip in GameAssets.i.soundAudioClipArray)
