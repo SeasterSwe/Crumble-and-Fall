22728aad48ed51a1edc44298c311e1f166b14747
Robert Sandh <sarobban@gmail.com>
2020-12-28 14:02:22 +0100

    FinalCountdown

--- CambleFallTesting/Assets/Scripts/GameFunction/SoundManager.cs
+++ CambleFallTesting/Assets/Scripts/GameFunction/SoundManager.cs
@@ -15,6 +15,13 @@ public static class SoundManager
         CannonHurtSound,
         CannonOutOfAmmo,
         BuilderPlacementSound,
+        count5,
+        count4,
+        count3,
+        count2,
+        count1
+
+
     }
     private static GameObject oneShotGameObject;
     private static AudioSource oneShotAudioSource;
