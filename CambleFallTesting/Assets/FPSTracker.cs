f636d9bacbb2098ffb225cb1c57dad8410e2c461
SeasterSwe <Jakobbulow01@gmail.com>
2021-01-04 20:19:50 +0100

    FPS Mätare

---
+++ CambleFallTesting/Assets/FPSTracker.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class FPSTracker : MonoBehaviour
+{
+    TextMeshProUGUI text;
+    void Start()
+    {
+        text = GetComponent<TextMeshProUGUI>();
+    }
+    float t = 0;
+    private void LateUpdate()
+    {
+        if (t < Time.time)
+        {
+            t = 0.356f + Time.time;
+            int fps = (int)(1f / Time.unscaledDeltaTime);
+            text.text = fps.ToString();
+        }
+
+    }
+}
